ecc6e50821b0d4ac91b7db0a78ffb6fb6d9b3af5
Respirayson <88478542+Respirayson@users.noreply.github.com>
2022-01-23 19:56:42 +0800

    Update InitializedProxy.sol

--- Contracts/Fractionalisation/InitializedProxy.sol
+++ Contracts/Fractionalisation/InitializedProxy.sol
@@ -1,10 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-/**
- * @title InitializedProxy
- * @author Anna Carroll
- */
 contract InitializedProxy {
     // address of logic contract
     address public immutable logic;
